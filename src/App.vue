9fa62195b582acd3fb345a8d25a2d2f0ee15a1ef
Tim Suvorov <timiniev@outlook.com>
2026-01-18 22:25:03 +0100

    feat: added a toast from primevue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import Toast from 'primevue/toast';
 import AppLayout from './components/layout/AppLayout.vue';
 </script>
 
 <template>
+  <Toast/>
   <AppLayout />
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
 import './assets/main.css'
-import PrimeVue from 'primevue/config';
+import PrimeVue from 'primevue/config'
+import Lara from '@primevue/themes/lara'
+
+
+
+import ToastService from 'primevue/toastservice'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-app.use(PrimeVue);
+app.use(PrimeVue, {
+  theme: {
+    preset: Lara
+  }
+})
+app.use(ToastService)
 app.use(createPinia())
 app.use(router)
-
-
 
 app.mount('#app')
