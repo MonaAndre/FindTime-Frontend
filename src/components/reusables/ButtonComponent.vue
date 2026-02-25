36bb209167a8cf1894d2512e31e0e5c7d28fe519
Tim Suvorov <timsuvorov@icloud.com>
2026-02-25 21:00:01 +0100

    fix: adjusted button design
    Co-authored-by: Mona <MonaAndre@users.noreply.github.com>

--- src/components/reusables/ButtonComponent.vue
+++ src/components/reusables/ButtonComponent.vue
@@ -28,20 +28,20 @@ defineProps<{
             'px-2.5 py-1.5 text-sm': md,
             'px-3 py-2 text-sm': lg,
             'p-2':roundedFull,
-            'bg-blue-600 text-white border-2 border-blue-500 hover:bg-blue-500 ':
+            'bg-blue-600 text-white border-2 border-blue-600 hover:bg-blue-500 ':
                 primary,
             ' text-blue-600 border-2 border-blue-600 hover:bg-blue-600 hover:text-white ':
                 secondary,
             ' text-red-600 border-2 border-red-600 hover:bg-red-600 hover:text-white ': danger,
 
-            'bg-gray-300 text-gray-700 border-2 hover:bg-gray-400 focus-visible:outline-gray-400 dark:bg-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-600 dark:focus-visible:outline-neutral-600':
+            'bg-gray-300 text-gray-700 border-2 border-gray-300 dark:border-neutral-700 hover:bg-gray-400 focus-visible:outline-gray-400 dark:bg-neutral-700 dark:text-gray-100 dark:hover:bg-neutral-600 dark:focus-visible:outline-neutral-600':
                 tertiary,
             'pointer-events-none cursor-not-allowed opacity-50': disabled,
             'pointer-events-none cursor-not-allowed': loading,
             'my-3': marginY,
             'mx-3': marginX,
             'rounded-full': roundedFull,
-            'rounded-md': !roundedFull
+            'rounded-xl': !roundedFull
         },
     ]" :disabled="disabled"
         class="inline-flex items-center cursor-pointer justify-center  font-semibold   focus-visible:outline-2 focus-visible:outline-offset-2">
